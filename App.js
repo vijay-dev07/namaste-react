a87a0d850dc725073832fdd728ab61ca43b4d2d7
vijay-dev07 <uk.kaithwas771@gmail.com>
2025-11-03 13:25:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
-const heading = React.createElement("h1" , {id:"heading"} , "Hellow World from React");
+
+const parent = React.createElement(
+    "div" , 
+    {id: "parent"} ,
+    React.createElement(
+        "div" ,
+        {id:"child"},
+        React.createElement("h1" , {} , "i m an h1 tag"),
+    )
+);
+
+// const heading = React.createElement("h1" ,
+//      {id:"heading" , xyz:"abc" } , "hellow world from react");
+console.log(parent);
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    root.render(parent);
